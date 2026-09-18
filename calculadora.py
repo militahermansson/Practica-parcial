c8f83e8cca79c6f96d0f600fc1f69f7bffebeeaa
milagros hermansson <militahermansson@gmail.com>
2026-09-18 16:40:27 -0300

    Mi primer commit

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+print("Calculadora en Python") 
+
+# Inicializar variables 
+numero1 = 4 
+numero2 = 6 
+
+# Sumar los números 
+suma = numero1 + numero2 
+
+# Restar los números
+resta = numero1 - numero2
+
+# Mostrar el resultado 
+print(f"El resultado de la suma es: {suma}") 
+print(f"El resultado de la resta es: {resta}") 
